fcf8b828943453b103104d6cab43da4993862e93
Grant Britchford <grant.britchford@hotmail.co.uk>
2024-04-30 09:30:29 +0000

    updated rutes.py, script.js and tasks.html

--- taskmanager/static/css/js/script.js
+++ taskmanager/static/css/js/script.js
@@ -10,4 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
    let selects= document.querySelectorAll("select");
    M.FormSelect.init(selects);
+
+   let collapsibles= document.querySelectorAll(".collapsible");
+   M.Collapsible.init(collapsibles);
 });
